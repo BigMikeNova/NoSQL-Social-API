748977682267e9f2aa846266c9050ea25054dfb7
BigMikeNova <125073452+BigMikeNova@users.noreply.github.com>
2023-06-29 19:22:19 -0400

    put, post, and delete routes for thoughts and users

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -27,4 +27,43 @@ router.get('/thoughts/:id', async (req, res) => {
   }
 });
 
+// POST route to create a new thought
+router.post('/thoughts', async (req, res) => {
+    try {
+      const newThought = await Thought.create(req.body);
+      res.status(201).json(newThought);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Server Error' });
+    }
+  });
+  
+  // PUT route to update a thought by ID
+  router.put('/thoughts/:id', async (req, res) => {
+    try {
+      const updatedThought = await Thought.findByIdAndUpdate(req.params.id, req.body, { new: true });
+      if (!updatedThought) {
+        return res.status(404).json({ message: 'Thought not found' });
+      }
+      res.json(updatedThought);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Server Error' });
+    }
+  });
+  
+  // DELETE route to delete a thought by ID
+  router.delete('/thoughts/:id', async (req, res) => {
+    try {
+      const deletedThought = await Thought.findByIdAndDelete(req.params.id);
+      if (!deletedThought) {
+        return res.status(404).json({ message: 'Thought not found' });
+      }
+      res.json({ message: 'Thought deleted successfully' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Server Error' });
+    }
+  });
+
 module.exports = router;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -27,6 +27,44 @@ router.get('/users/:id', async (req, res) => {
     res.status(500).json({ message: 'Server Error' });
   }
 });
+// POST route to create a new user
+router.post('/users', async (req, res) => {
+    try {
+      const newUser = await User.create(req.body);
+      res.status(201).json(newUser);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Server Error' });
+    }
+  });
+  
+  // PUT route to update a user by ID
+  router.put('/users/:id', async (req, res) => {
+    try {
+      const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+      if (!updatedUser) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      res.json(updatedUser);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Server Error' });
+    }
+  });
+  
+  // DELETE route to delete a user by ID
+  router.delete('/users/:id', async (req, res) => {
+    try {
+      const deletedUser = await User.findByIdAndDelete(req.params.id);
+      if (!deletedUser) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      res.json({ message: 'User deleted successfully' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Server Error' });
+    }
+  });
 
 
 module.exports = router;
