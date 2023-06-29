a67ffc6a0785e258fcd5061aa655cdeac38edf75
BigMikeNova <125073452+BigMikeNova@users.noreply.github.com>
2023-06-29 19:19:32 -0400

    model code

---
+++ models/Thought.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const thoughtSchema = new mongoose.Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+module.exports = Thought;
